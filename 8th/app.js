8c10e30038371197b0221666b7e7f9ee3f32a951
Huigyeong Na <nhk1007@naver.com>
2025-02-25 23:38:32 +0900

    Day30 - 변수 선언 및 값 할당 / JS 파일 outsourcing / array 및 object

---
+++ 8th/app.js
@@ -0,0 +1,17 @@
+/*let greetingText, int;
+greetingText = 'Hello, world!';
+int = 123;
+alert(greetingText);
+alert(int);
+greetingText = 'asdf';
+alert(greetingText);*/
+
+
+let age = 22;
+let userName = 'HG';
+
+let hobbies = ['Sports', 'Cooking', 'Reading', 1, 2];
+alert(hobbies);           /* Output: Sports,Cooking,Reading,1,2 */
+alert(hobbies[0]);        /* Output: Sports */
+
+let job = {title: 'Developer', place: 'Seoul', salary: 99999};
