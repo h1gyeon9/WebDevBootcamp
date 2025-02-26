012faa124e3e12c8abc53ef9ae44d7e902cc3bf3
Huigyeong Na <nhk1007@naver.com>
2025-02-26 15:41:59 +0900

    Day31 - function, parameter, return 실습

--- 8th/app.js
+++ 8th/app.js
@@ -14,4 +14,21 @@ let hobbies = ['Sports', 'Cooking', 'Reading', 1, 2];
 alert(hobbies);           /* Output: Sports,Cooking,Reading,1,2 */
 alert(hobbies[0]);        /* Output: Sports */
 
-let job = {title: 'Developer', place: 'Seoul', salary: 99999};
+let job = {
+    title: 'Developer',
+    place: 'Seoul',
+    salary: 99999
+};
+alert(job.title);         /* Output: Developer */
+
+let adultYears;
+
+adultYears = age - 18;
+alert(adultYears);
+
+function calculateAdultYears(userAge) {
+    return userAge - 18;
+}
+
+let totalAdultYears = calculateAdultYears(100);
+alert(totalAdultYears);
