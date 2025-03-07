2a231eea424bf135c36426735487c29004b2d38a
Huigyeong Na <nhk1007@naver.com>
2025-03-07 15:31:38 +0900

    Day36 - 예제 페이지 제작 / const 상수 / JS로 CSS class 관리 및 style 변경

---
+++ 11th/app.js
@@ -0,0 +1,30 @@
+/*let userText;
+let textLength = 0;
+let lengthOutput = document.createElement('p')
+let inputElement = document.body.querySelector('input');
+let divElement = document.body.querySelector('div');
+
+
+function saveUserText(){
+    userText = inputElement.value;
+    textLength = userText.length;
+    lengthOutput.textContent = textLength + '/60'
+}
+
+inputElement.addEventListener('input', saveUserText);
+
+divElement.append(lengthOutput);*/
+
+let inputElement = document.body.querySelector('input');
+let maxAllowedChars = inputElement.maxLength;
+
+inputElement.addEventListener('input', calculateRemainingChars);
+
+function calculateRemainingChars(){
+    let userText = inputElement.value;
+    let textLength = userText.length;
+
+    let remainingChars = maxAllowedChars - textLength;
+
+    document.body.querySelector('#remaining-chars').textContent = remainingChars;
+}
